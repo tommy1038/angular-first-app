e7f00f073c99934f8c7108c733a79dbed4c78a8c
tommy1038 <r.tommy1038@gmail.com>
2025-06-18 00:05:38 +0900

    feat: 07-dynamic-template-values

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -2,9 +2,17 @@ import {Component, input} from '@angular/core';
 import {HousingLocationInfo} from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
-  imports: [],
   template: `
-    <p>housing-location works!</p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+    </section>
   `,
   styleUrls: ['./housing-location.css'],
 })
