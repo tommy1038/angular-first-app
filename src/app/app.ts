32c911aae42f5488e63b1fffc498572b62de8efb
tommy1038 <r.tommy1038@gmail.com>
2025-06-19 23:04:55 +0900

    feat: 10-routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import {Component} from '@angular/core';
-import {Home} from './home/home';
+import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
